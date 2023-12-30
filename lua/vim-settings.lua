c4708dff968ae8db622318899eb336d57723ba80
MuhammedShahin <mohamedshahin796@gmail.com>
2023-12-31 01:35:33 +0200

    add some custom remaps

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -3,4 +3,16 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
 vim.g.mapleader = " "
+--Enalbe system clipboard integration
+vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
+
+--Disable search highlighting 
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
