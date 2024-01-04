c8c011a55e88d3fc9782d60d67f98e74cbab3336
MuhammedShahin <mohamedshahin796@gmail.com>
2024-01-04 12:34:25 +0200

    resolve snippet issue by removing vsnip calling in luaSnip expand

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -20,7 +20,6 @@ return {
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						vim.fn["vsnip#anonymous"](args.body)
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
@@ -31,10 +30,10 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-leader>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
+			}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
