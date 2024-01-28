59d78badb421135e5829cceebd996578903fbaca
MuhammedShahin <mohamedshahin796@gmail.com>
2024-01-28 23:40:03 +0200

    do better settings and remaps

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -1,19 +1,24 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
-
-vim.g.mapleader = " "
---Enalbe system clipboard integration
-vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
-
 --Disable search highlighting
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.g.mapleader = " "
+--Enable system clipboard integration
+vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
+
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
